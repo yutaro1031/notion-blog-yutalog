f69d543e7d35f130a8f7018db03eb2e9eed48b62
youchann <yutaro50@gmail.com>
2021-03-06 17:38:42 +0900

    Add error boundary

---
+++ src/components/boundaries/errorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component, ErrorInfo } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Sorry... Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/global.css";
 import Footer from "../components/atom/footer";
 import { AppProps } from "next/app";
+import { ErrorBoundary } from "../components/boundaries/errorBoundary";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
+  <ErrorBoundary>
     <Component {...pageProps} />
     <Footer />
-  </>
+  </ErrorBoundary>
 );
 
 export default App;
